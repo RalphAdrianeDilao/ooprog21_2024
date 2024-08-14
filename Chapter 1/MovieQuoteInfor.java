05c651ce37c0e6146123058bc595fffa2d281be0
AdrianeDriane <dilaoaparre@gmail.com>
2024-08-14 20:16:22 +0800

    MovieQuoteInfor.java file

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,6 @@
+public class MovieQuoteInfor {
+    public static void main(String[] args) {
+        System.out.println("\"It ain't about how hard you can hit. It's about how hard you can get hit and keep moving forward.\"");
+        System.out.println("- Adonis Creed, 2015");
+    }
+}
