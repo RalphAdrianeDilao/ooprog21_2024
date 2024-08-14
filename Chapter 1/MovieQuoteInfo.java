ca4f0740e8cf811f92f389c71bb5781288b708c9
AdrianeDriane <dilaoaparre@gmail.com>
2024-08-14 20:37:02 +0800

    Rename MovieQuoteInfor to MovieQuoteInfo

--- Chapter 1/MovieQuoteInfo.java
+++ Chapter 1/MovieQuoteInfo.java
@@ -1,6 +1,6 @@
-public class MovieQuoteInfor {
+public class MovieQuoteInfo {
     public static void main(String[] args) {
         System.out.println("\"It ain't about how hard you can hit. It's about how hard you can get hit and keep moving forward.\"");
-        System.out.println("- Adonis Creed, 2015");
+        System.out.println("- Creed, Adonis Creed, 2015");
     }
 }
