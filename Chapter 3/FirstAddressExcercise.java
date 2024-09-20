01e5eaabe4fc0e006aeab754d7256bfd7ffac49c
RalphAdrianeDilao <dilao_marites@yahoo.com>
2024-09-20 09:16:30 +0700

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExercise{
+    public static void main(String[] args){
+        displayAddress();
+    }
+    
+    public static void displayAddress(){
+        System.out.println("Lenovo Group Limited");
+        System.out.println("23rd Floor, Lincoln House, Taikoo Place");
+        System.out.println("979 King's Road, Quarry Bay, Hong Kong S.A.R. of China");
+        System.out.println("Company: Lenovo");
+    }
+}
