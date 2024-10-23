5a38cbbfaeb90dd0e1792e2387430170b84f69bf
RalphAdrianeDilao <dilao_marites@yahoo.com>
2024-10-23 23:25:14 +0800

    Create Sort.java

---
+++ Chapter 5/Sort.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Sort {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input the first number: ");
+        int first = scan.nextInt();
+        System.out.print("Input the second number: ");
+        int second = scan.nextInt();
+        System.out.print("Input the third number: ");
+        int third = scan.nextInt();
+
+        boolean isAlreadySorted = false;
+
+        if (first < second && second < third) {
+            System.out.println("Input is already in ascending order. Printing in ascending order.");
+            System.out.printf("Ascending Order: %d, %d, %d", first, second, third);
+        } else {
+            System.out.println("Printing in descending order.");
+            int temp;
+
+            if (third > second) {
+                temp = second;
+                second = third;
+                third = temp;
+            }
+
+            if (third > first) {
+                temp = first;
+                first = third;
+                third = temp;
+            }
+
+            if (second > first) {
+                temp = first;
+                first = second;
+                second = temp;
+            }
+
+            System.out.printf("Descending Order: %d, %d, %d", first, second, third);
+        }
+    }
+}
